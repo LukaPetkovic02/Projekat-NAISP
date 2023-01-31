da1715f9ba8565671d7b2e689a8f68376f1f9089
StefanDjuricaSV35-2021 <stefandjurica420@gmail.com>
2023-01-31 20:10:07 +0100

    Dodat Deserialize

--- wal/wal.go
+++ wal/wal.go
@@ -1,10 +1,13 @@
 package wal
 
 import (
+	"io"
+	"log"
 	"os"
 
 	"github.com/LukaPetkovicSV16/Projekat-NAISP/engine"
 	"github.com/LukaPetkovicSV16/Projekat-NAISP/types"
+	"github.com/edsrzf/mmap-go"
 )
 
 var segment_size int = 3
@@ -15,12 +18,55 @@ func Append(record types.Record) bool {
 	if err != nil {
 		panic(err)
 	}
-	// Write to the file
-	file.Write(record.Serialize())
+	current_data := ReadWalSegment(*file)
+
+	if len(current_data) >= segment_size {
+		file.Close()
+		file, err = os.OpenFile(engine.GetNextWalFilePath(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+		return Append(record)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	file_size := info.Size()
+
+	err = file.Truncate(file_size + 29 + (int64)(record.KeySize) + (int64)(record.ValueSize))
+	mmapFile, err := mmap.Map(file, mmap.RDWR, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	copy(mmapFile[file_size:], record.Serialize())
+	mmapFile.Unmap()
 	file.Close()
+
 	return true
 }
 
 func Clear() {
-	//treba da obrise sve wal fajlove
+	os.RemoveAll(engine.GetWriteAheadLogDir())
+	os.Mkdir(engine.GetWriteAheadLogDir(), 0755)
+}
+
+func ReadWalSegment(file os.File) []types.Record {
+	log, err := io.ReadAll(&file)
+	if err != nil {
+		panic(err)
+	}
+
+	var ret []types.Record
+	var current_position int64 = 0
+
+	for i := 0; current_position < (int64)(len(log)) && i < segment_size; i++ {
+		ret = append(ret, types.DeserializeRecord(log[current_position:]))
+		current_position += int64(len(ret[len(ret)-1].Serialize()))
+	}
+
+	return ret
 }
