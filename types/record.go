ff0f4e47ed252cf79c1771bd0f06b53f9a1ed90b
Luka Petkovic <lukapetkovicns21@gmail.com>
2023-01-31 09:54:11 +0100

    izmena

---
+++ types/record.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+	"time"
+)
+
+const (
+	CRC_SIZE        = 4
+	TIMESTAMP_SIZE  = 8
+	TOMBSTONE_SIZE  = 1
+	KEY_SIZE_SIZE   = 8
+	VALUE_SIZE_SIZE = 8
+)
+
+type Record struct {
+	CRC       uint32
+	Timestamp uint64
+	Tombstone bool
+	KeySize   uint64
+	ValueSize uint64
+	Key       []byte
+	Value     []byte
+}
+
+func CreateRecord(key string, value []byte, tombstone bool) Record {
+	return Record{
+		CRC:       crc32.ChecksumIEEE(value),
+		Timestamp: uint64(time.Now().UnixMilli()),
+		Tombstone: tombstone,
+		KeySize:   uint64(len(key)),
+		ValueSize: uint64(len(value)),
+		Key:       []byte(key),
+		Value:     value,
+	}
+}
+
+func (record Record) Serialize() []byte {
+	var serializedRecord = new(bytes.Buffer)
+
+	binary.Write(serializedRecord, binary.LittleEndian, record.CRC)
+	binary.Write(serializedRecord, binary.LittleEndian, record.Timestamp)
+	binary.Write(serializedRecord, binary.LittleEndian, record.Tombstone)
+	binary.Write(serializedRecord, binary.LittleEndian, record.KeySize)
+	binary.Write(serializedRecord, binary.LittleEndian, record.ValueSize)
+	binary.Write(serializedRecord, binary.LittleEndian, record.Key)
+	binary.Write(serializedRecord, binary.LittleEndian, record.Value)
+	return serializedRecord.Bytes()
+}
+
+func DeserializeRecord(serializedRecord []byte) Record {
+	// TODO: Implement this function
+	return Record{}
+}
