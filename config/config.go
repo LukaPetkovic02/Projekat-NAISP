70e4e929777912fe4a7b81718ae06920f149f4ae
Mladen Skrbic <mladenskrbiclamba3@gmail.com>
2023-02-03 12:41:46 +0100

    added default config in code

--- config/config.go
+++ config/config.go
@@ -5,7 +5,26 @@ func Get() *Config {
 	if config != nil {
 		return config
 	} else {
-		return &Config{}
+		return &Config{
+			Structure: "multiple-files",
+			BloomFilter: BloomFilter{
+				Precision: 0.01,
+			},
+			Cache: Cache{
+				Size: 10,
+			},
+			SkipList: SkipList{
+				MaxLevel: 7,
+			},
+			Lsm: Lsm{
+				MaxLevel: 5,
+			},
+			TokenBucket: TokenBucket{
+				Size: 10,
+				Rate: 1000,
+			},
+		}
+
 	}
 
 }
--- config/external.go
+++ config/external.go
@@ -8,23 +8,28 @@ import (
 )
 
 type Config struct {
-	Structure   string `yaml:"structure"`
-	BloomFilter struct {
-		Precision float64 `yaml:"precision"`
-	} `yaml:"bloom_filter"`
-	Cache struct {
-		Size uint `yaml:"size"`
-	} `yaml:"cache"`
-	SkipList struct {
-		MaxLevel uint `yaml:"max_level"`
-	} `yaml:"skip_list"`
-	Lsm struct {
-		MaxLevel uint `yaml:"max_level"`
-	}
-	TokenBucket struct {
-		Size uint   `yaml:"size"`
-		Rate uint64 `yaml:"rate"`
-	} `yaml:"token_bucket"`
+	Structure   string      `yaml:"structure"`
+	BloomFilter BloomFilter `yaml:"bloomFilter"`
+	Cache       Cache       `yaml:"cache"`
+	SkipList    SkipList    `yaml:"skipList"`
+	Lsm         Lsm         `yaml:"lsm"`
+	TokenBucket TokenBucket `yaml:"tokenBucket"`
+}
+type BloomFilter struct {
+	Precision float64 `yaml:"precision"`
+}
+type Cache struct {
+	Size uint `yaml:"size"`
+}
+type SkipList struct {
+	MaxLevel uint `yaml:"maxLevel"`
+}
+type Lsm struct {
+	MaxLevel uint `yaml:"maxLevel"`
+}
+type TokenBucket struct {
+	Size uint   `yaml:"size"`
+	Rate uint64 `yaml:"rate"`
 }
 
 func load() *Config {
